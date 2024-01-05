d3db56701c0b55ef6062dae64c1d13d3d0267fd5
willzhen <willzhen@tencent.com>
2024-01-05 16:56:44 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup, find_packages
 #This is a list of files to install, and where
 #(relative to the 'root' dir, where setup.py is)
 #You could be more specific.
-files = ["protobufspec/*", "trpc_mock/*"]
 
 setup(name = "aimediasdk",
-    version = "100",
+    version = "1.0.0",
     description = "tencent 媒体AI中台 sdk",
     author = "willzhen",
     author_email = "willzhen@tencent.com",
-    url = "",
+    url = "https://github.com/Tencent-media-asset-system-sdk/media-platform-sdk-python",
     #Name the folder where your packages live:
     #(If you have other packages (dirs) or modules (py files) then
     #put them into the package directory - they will be found
@@ -20,7 +19,6 @@ setup(name = "aimediasdk",
     #I called the package 'package' thus cleverly confusing the whole issue...
     #This dict maps the package name =to=> directories
     #It says, package *needs* these files.
-    package_data = {'package' : files },
     #'runner' is in the root.
     # scripts = ["runner"],
     # long_description = """Really long text here.""",
